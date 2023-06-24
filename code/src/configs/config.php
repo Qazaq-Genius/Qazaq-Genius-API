daa1a5a2ea45d7b5707e0e2be651743bb3040f59
Anatolij Vasilev <Anatolij.Vasilev@flyeralarm.com>
2023-06-24 15:02:09 +0200

    #14 added db to .env variable

--- code/src/configs/config.php
+++ code/src/configs/config.php
@@ -1,7 +1,7 @@
 <?php
 
 define("DB_HOST", ENV['MYSQL_HOST']);
-define("DB_NAME", 'qg_song_data');
+define("DB_NAME", ENV['MYSQL_DB']);
 define("DB_PORT", '3306');
 define("DB_USER", ENV['MYSQL_USER']);
 define("DB_PASS", ENV['MYSQL_ROOT_PASSWORD']);
